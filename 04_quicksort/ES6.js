9f723cb2f65e062ff7d1baca07a1b3a1c0e13fe0
zone <oospace@foxmail.com>
2021-08-26 15:17:12 +0800

    变更命名

--- 04_quicksort/ES6.js
+++ 04_quicksort/ES6.js
@@ -1,7 +1,6 @@
 const quicksort = (list) => {
-	if (list.length < 2) {
-		return list;
-	} else {
+	if (list.length < 2) return list;
+	else {
 		let pivot = list[0];
 		let lessArr = [], greaterArr = [];
 		for (let i = 1; i < list.length; i++) {
@@ -11,8 +10,8 @@ const quicksort = (list) => {
 				greaterArr.push(list[i]);
 			}
 		}
-		return [...quicksort(lessArr),pivot,...quicksort(greaterArr)]
+		return [...quicksort(lessArr), pivot, ...quicksort(greaterArr)];
 	}
 }
 
-console.log(quicksort([2,1,4,5,6,0,7,9,3,2,8]))
+console.log(quicksort([2, 1, 4, 5, 6, 0, 7, 9, 3, 2, 8]))
