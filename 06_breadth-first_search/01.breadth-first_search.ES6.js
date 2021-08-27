d41575804cf8fd8c6dbc31ed9016373fe638966f
zone <oospace@foxmail.com>
2021-08-27 14:23:22 +0800

    增加链接路线

--- 06_breadth-first_search/01.breadth-first_search.ES6.js
+++ 06_breadth-first_search/01.breadth-first_search.ES6.js
@@ -1,24 +1,25 @@
 const graph = {
-	you: ["alice", "bob", "claire"],
-	bob: ["anuj", "peggy", "you"],
-	alice: ["peggy", "you"],
-	claire: ["thom", "jonny", "you"],
+	joe: ["alice", "bob", "claire"],
+	bob: ["anuj", "peggy", "joe"],
+	alice: ["peggy", "joe"],
+	claire: ["thom", "jonny", "joe"],
 	peggy: [],
 	thom: ["tina"],
-	jonny: []
+	jonny: [],
+	tina: []
 };
 
 const personIsSeller = (name, seller) => name[name.length - 1] === seller;
 
-let searchCache = {}
+let searchCache = {};
+let linkWay = [];
 const breadth_firstSearch = (name, seller) => {
 	searchCache[name] = true
 	let searchQueue = graph[name] || [];
-	
-	console.log(searchQueue)
-	
 	for (let i = 0; i < searchQueue.length; i++) {
 		if (personIsSeller(searchQueue[i], seller)) {
+			linkWay.unshift(name)
+			linkWay.push(searchQueue[i])
 			return searchQueue[i];
 		}
 	}
@@ -27,13 +28,15 @@ const breadth_firstSearch = (name, seller) => {
 		if (!searchCache[searchQueue[i]]) {
 			let searchUser = breadth_firstSearch(searchQueue[i], seller);
 			if (searchUser != "noBody") {
+				linkWay.unshift(name)
 				return searchUser
 			}
 		}
 	}
+	
 	return "noBody"
 }
 
-//找到you的朋友或者朋友的朋友里以字母 ${seller} 结尾的人
+//找到某人X的朋友或者朋友的朋友里以字母 ${seller} 结尾的人
 const seller = "a"
-console.log(`${breadth_firstSearch("alice", seller)} is the seller`)
+console.log(`\n${breadth_firstSearch("alice", seller)} is the seller`, `\n\n${linkWay.join('->')}`)
